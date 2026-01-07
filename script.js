50d763180dd4ab437c29e842673da4f72d5d718b
HankChen <45835758+maybeimcat86211@users.noreply.github.com>
2026-01-07 10:59:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+console.log('script.js loaded');
 // ==================== 修改這兩行 ====================
 const SUPABASE_URL = 'https://feegzkbrumieucyweghm.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_B_taCjibUltphJ-1jmmWYQ_8__FYb45';
